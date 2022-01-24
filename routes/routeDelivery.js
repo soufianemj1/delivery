554a11eba9f3c16c27cde8ecc7deebb3a90586ea
soufianemj1 <elmajati.s@gmail.com>
2022-01-24 16:43:13 +0100

    logs

--- routes/routeDelivery.js
+++ routes/routeDelivery.js
@@ -1,8 +1,9 @@
 const Router = require("express").Router()
 const delivery = require("../controllers/deliveryController")
 const morgan = require("morgan")
+const fs = require("fs")
 
-Router.post("/create", morgan('combined'), delivery.create)
+Router.post("/create", morgan('common', {stream: fs.createWriteStream('./admin.log', {flags: 'a'})}), delivery.create)
 Router.get("/findall",morgan('combined'), delivery.findAll)
 Router.put("/update/:id",morgan('combined'),delivery.update)
 Router.delete("/delete/:id",morgan('combined'),delivery.delete)
